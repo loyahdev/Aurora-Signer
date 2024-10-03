a41463001b248c4b562161879eb5995fd9f8b923
KnowinApps <87503170+Jaxon10117@users.noreply.github.com>
2024-10-02 22:55:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -508,13 +508,13 @@ document.addEventListener("DOMContentLoaded", function () {
         updateUIForUser(currentUser);
     });
 
-    /*document.getElementById('toggleAuthPassword').addEventListener('click', function() {
+    document.getElementById('toggleAuthPassword').addEventListener('click', function() {
         const passwordInput = document.getElementById('auth-password');
         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
         passwordInput.setAttribute('type', type);
         this.classList.toggle('fa-eye');
         this.classList.toggle('fa-eye-slash');
-    });*/
+    });
 
     function showPrivacyPolicy() {
         document.getElementById('authForm').classList.add('hidden');
